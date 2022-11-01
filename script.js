f8459bbf869efdcea2ac3a6de0c284b33d2a3e5d
0xmav <0xmav.eth@gmail.com>
2022-11-01 11:37:28 +0800

    "Commit of the ititial version v.1"

---
+++ script.js
@@ -0,0 +1,20 @@
+let parent;
+
+const observer = new MutationObserver(function (mutations, mutationInstance) {
+    let element = document.querySelectorAll("[computed-object-fit=CONTAIN]");
+    if (element) {
+        OnlyLongs(element, parent);
+    }
+});
+
+observer.observe(document, {
+    childList: true,
+    subtree:   true
+});
+
+function OnlyLongs(element, parent) {
+	for (let i = 0; i < element.length; i++) {
+		parent = element[i].closest("[lockup=true]");
+		parent.style.display = "none";
+	}
+}
