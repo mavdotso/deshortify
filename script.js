d5e7a86b4313f681d33ba50004a30cf63a948289
mav <0xmav.eth@gmail.com>
2024-09-06 21:21:09 +0400

    fix: new element removal

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 let parent;
 
 const observer = new MutationObserver(function (mutations, mutationInstance) {
-    let element = document.querySelectorAll("[overlay-style=SHORTS]");
-    if (element) {
-        OnlyLongs(element, parent);
+    let shortsElement = document.querySelectorAll('[overlay-style=SHORTS]');
+    if (shortsElement) {
+        OnlyLongs(shortsElement, parent);
+    }
+
+    let gridGroupElement = document.querySelector('ytd-rich-grid-group.YtdRichGridGroupHost');
+    if (gridGroupElement) {
+        removeYtdRichGridGroup(gridGroupElement);
     }
 });
 
 observer.observe(document, {
     childList: true,
-    subtree:   true
+    subtree: true,
 });
 
 function OnlyLongs(element, parent) {
-	for (let i = 0; i < element.length; i++) {
-		parent = element[i].closest("[lockup=true]");
-		parent.style.display = "none";
-	}
+    for (let i = 0; i < element.length; i++) {
+        parent = element[i].closest('[lockup=true]');
+        parent.style.display = 'none';
+    }
+}
+
+function removeYtdRichGridGroup(element) {
+    if (element) {
+        element.remove();
+        console.log('ytd-rich-grid-group element removed successfully');
+    }
 }
